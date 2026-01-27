d66f83127af28e546acde32479799c40250bb0d8
joshuaphewitt <joshhewitt11@live.co.uk>
2026-01-27 18:51:51 +0000

    first_changes/test.py

---
+++ first_changes/test.py
@@ -0,0 +1,10 @@
+
+class file:
+    def __init__(self, filetype, filepath):
+        self.filetype = filetype
+        self.filepath = filepath
+    
+class stream:
+    def __init__(self, filetype, url):
+        self.filetype = filetype
+        self.url = url
